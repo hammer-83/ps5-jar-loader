c37b74a0efdc2bc98fecac2a1163deb296383635
hammer-83 <48300721+hammer-83@users.noreply.github.com>
2025-01-27 14:38:30 -0500

    Firmware independent detection of process pmap offset

--- sdk/src/main/java/org/ps5jb/sdk/include/vm/map/VmSpace.java
+++ sdk/src/main/java/org/ps5jb/sdk/include/vm/map/VmSpace.java
@@ -1,17 +1,21 @@
 package org.ps5jb.sdk.include.vm.map;
 
+import org.ps5jb.sdk.core.SdkSoftwareVersionUnsupportedException;
 import org.ps5jb.sdk.core.kernel.KernelPointer;
+import org.ps5jb.sdk.include.inet.in.ProtocolType;
 import org.ps5jb.sdk.include.machine.pmap.PhysicalMap;
 import org.ps5jb.sdk.include.machine.pmap.PhysicalMapFlag;
 import org.ps5jb.sdk.include.machine.pmap.PhysicalMapType;
 import org.ps5jb.sdk.include.sys.mutex.MutexType;
+import org.ps5jb.sdk.res.ErrorMessages;
 
 /**
  * Incomplete wrapper for FreeBSD <code>vmspace</code> structure.
  */
 public class VmSpace {
     public static final long OFFSET_VM_MAP = 0;
-    public static final long OFFSET_VM_PMAP = 704L;
+    /** Firmware-dependent offset to pmap from the start of the VmSpace pointer. If -1, then the offset could not be determined for the current firmware version. */
+    public static long OFFSET_VM_PMAP = -1;
 
     private final KernelPointer ptr;
 
@@ -39,6 +43,7 @@ public class VmSpace {
      * @return Returns the value of <code>vm_pmap</code> field of <code>pmap</code> structure.
      */
     public PhysicalMap getPhysicalMap() {
+        detectVmPmapOffset();
         return new PhysicalMap(getPointer().inc(OFFSET_VM_PMAP));
     }
 
@@ -49,5 +54,35 @@ public class VmSpace {
      */
     public KernelPointer getPointer() {
         return this.ptr;
+    }
+
+    /**
+     * Offset to <code>vm_pmap</code> is firmware dependent. This method does a one-time detection
+     * of this value.
+     */
+    private void detectVmPmapOffset() {
+        if (OFFSET_VM_PMAP == -1) {
+            synchronized (VmSpace.class) {
+                if (OFFSET_VM_PMAP == -1) {
+                    // Note, this is the offset of vm_space.vm_map.pmap on 1.xx.
+                    // It is assumed that on higher firmwares it's only increasing.
+                    long curScanOffset = 0x1C8;
+                    for (int i = 0; i < 5; ++i) {
+                        long scanVal = this.ptr.read8(curScanOffset + i * 8L);
+                        long offsetDiff = scanVal - this.ptr.addr();
+                        // See if the value is an offset within VmSpace structure.
+                        // The expected offset range below is confirmed to be accurate up to 7.xx.
+                        if (offsetDiff >= 0x2C0 && offsetDiff <= 0x2F0) {
+                            OFFSET_VM_PMAP = offsetDiff;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (OFFSET_VM_PMAP == -1) {
+                throw new SdkSoftwareVersionUnsupportedException(ErrorMessages.getClassErrorMessage(VmSpace.class,"offsetVmPmapUnknown"));
+            }
+        }
     }
 }
